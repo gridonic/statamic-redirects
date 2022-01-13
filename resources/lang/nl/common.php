4bd77d65699b4d34ae1b64ead09d7b0539b3d9cd
Cees van Egmond <cees.vanegmond@wearejust.com>
2022-01-13 11:43:25 +0100

    Feature: Add NL

---
+++ resources/lang/nl/common.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'manual_redirects' => 'Handmatige redirects ',
+    'manual_redirect_create' => 'Redirect toevoegen',
+    'manual_redirect_edit' => 'Redirect wijzigen',
+    'auto_redirects' => 'Automatische redirects',
+    'monitor_404' => '404 Monitor',
+    'from' => 'Van',
+    'to' => 'Naar',
+    'target_type' => 'Doeltype',
+    'status_code' => 'Status Code',
+    'start_date' => 'Startdatum/-tijd',
+    'end_date' => 'Einddatum/-tijd',
+    'hits' => 'Hits',
+];
